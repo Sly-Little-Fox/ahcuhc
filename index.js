969e7094e6db602dc3e47bd761f0f5fc7f1ec370
Sly-Little-Fox <69681150+Sly-Little-Fox@users.noreply.github.com>
2020-12-11 19:40:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,83 @@
+#!/usr/bin/node
+
+// * Ahcuhc
+
+process.on('uncaughtException', function (error) {
+  errors.push(error.stack);
+});
+
+var errors = [];
+var chuchas = [];
+
+const system = require('child_process');
 const crypto = require('crypto');
 const path = require('path');
 const fs = require('fs');
-const home = require('os').homedir();
-const directoryPath = path.join(home, 'AppData', 'Local', '{2KFlBIxZE79fDMWxoKEr}');
-fs.readdir(directoryPath, (err, files) => {
-  if (err) {
-    console.log('Unable to scan directory: ', err);
-  }
-  files.forEach((file) => {
-    const hash = crypto.createHash('sha1');
-    const input = fs.createReadStream(file);
-    input.on('readable', () => {
-      const data = input.read();
-      if (data)
-        hash.update(data);
-      else {
-        if(hash.digest('hex') === 'c9ab0ba1d92e2f755301ef975c0990fcdb321805') {
-          console.log(`Found Chucha in ${file}!`);
+// ^^ Chucha test file ^^
+const SUM = 'c9ab0ba1d92e2f755301ef975c0990fcdb321805';
+
+function log(text) {
+  system.execSync(`echo "${text}" >> /tmp/ahcuhc.log`);
+}
+
+function read(dir) {
+  try {
+    fs.readdirSync(dir).forEach(function (fileordir) {
+      if (fs.lstatSync(path.join(dir, fileordir)).isDirectory()) {
+        try {
+          read(path.join(dir, fileordir));
+        } catch (error) {
+          console.warn(error);
+        }
+      } else {
+        if (fs.statSync(path.join(dir, fileordir)).size === 23) {
+          var hash = crypto.createHash('sha1').update(fs.readFileSync(path.join(dir, fileordir))).digest('hex');
+          if (hash === SUM) {
+            chuchas.push(path.join(dir, fileordir));
+          }
         }
       }
     });
-  });
-});
+  } catch (error) {
+    errors.push(error);
+  }
+
+  try {
+    read(__dirname);
+  } catch (error) {
+    errors.push(error);
+  }
+}
+
+if (chuchas == 0) {
+  system.execSync(`notify-send --icon="/home/all/Pictures/shield.svg" "No chuchas found! Good for you!"`);
+} else {
+  try {
+    try {
+      system.execSync(`mkdir /var/tmp/ahcuhc_temp/`);
+    } catch (error) {
+      log(error);
+    }
+    system.execSync(`rm -f ${path.join(__dirname, 'quarantine.tar')}`);
+    log('Removed old quarantine!');
+    system.execSync(`rm -f /tmp/ahcuhc.log`);
+    log('Removed old log!');
+    for (let i = 0; i < chuchas.length; i++) {
+      system.execSync(`mv ${chuchas[i]} /var/tmp/ahcuhc_temp/chucha-${i}.ahcuhc`);
+    }
+
+    try {
+      system.execSync(`cd /var/tmp/ahcuhc_temp/ && tar -cf "${path.join(__dirname, 'quarantine.tar')}" * && rm -rf "/var/tmp/ahcuhc_temp/"`);
+    } catch (error) {
+      log(error);
+      errors.push(error);
+    }
+    log('Quarantined chuchas!');
+    system.execSync(`notify-send --icon="/home/all/Pictures/shield.svg" "Quarantine file with ${chuchas.length} chucha(s) created in ${__dirname}!"`);
+  } catch (error) {
+    errors.push(error);
+  }
+
+}
+
+console.warn(`Errors: ${errors}`);
